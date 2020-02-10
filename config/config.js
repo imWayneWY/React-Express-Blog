631980d0d0d051cfa2b6e206093c18c97225529d
weiyan <im.weiyan@foxmail.com>
2020-02-10 14:16:46 -0700

    add readme.md

--- config/config.js
+++ config/config.js
@@ -1,11 +1,11 @@
 let HOST,PORT,APIHOST,APIPORT,NODE_ENV;
 module.exports = {
-    // host: HOST || '127.0.0.1',
-    // port: PORT || (NODE_ENV === 'production'?8080:3010),
-    host: HOST || '123.56.226.100',
-    port: PORT || 3010,
-    apiHost: APIHOST || '123.56.226.100',
-    apiPort: APIPORT || '3010',
+    host: HOST || '127.0.0.1',
+    port: PORT || (NODE_ENV === 'production'?8080:3010),
+    // host: HOST || '123.56.226.100',
+    // port: PORT || 3010,
+    apiHost: APIHOST || '127.0.0.1',
+    apiPort: APIPORT || '3020',
     dbHost: "localhost",
     dbPort: "27017",
     app: {
--- src/fetch/fetch.js
+++ src/fetch/fetch.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 let config = {
-    baseURL: 'http://127.0.0.1:3020', //develop
+    baseURL: 'http://123.56.226.100:82', //服务器ip
     //baseURL: '/api',                    //final
     transformRequest: [
         function(data) {
